c4c023a722cef4e2990ae9607191fb3b80d40038
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2022-11-21 00:32:43 +0100

    Add tests

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+import pandas as pd
+from classification_model.processing.data_manager import load_data
+from classification_model.config.core import config
+
+
+@pytest.fixture()
+def sample_test_input_data() -> pd.DataFrame:
+    """This returns the test data as a DF for testing."""
+    data = load_data(filepath=config.app_config.test_data_file)
+    return data
