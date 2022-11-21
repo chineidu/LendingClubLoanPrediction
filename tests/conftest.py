554689c1c14873ba7085f5e6b7954993b408f5f2
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2022-11-21 08:09:45 +0100

    Add sample_train_data

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,9 @@ def sample_test_input_data() -> pd.DataFrame:
     """This returns the test data as a DF for testing."""
     data = load_data(filepath=config.app_config.test_data_file, is_train=False)
     return data
+
+@pytest.fixture()
+def sample_train_input_data() -> pd.DataFrame:
+    """This returns the train data as a DF for testing."""
+    data = load_data(filepath=config.app_config.sample_train_data_file, is_train=True)
+    return data
