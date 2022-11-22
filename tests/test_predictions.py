99e1827c2692dbf2f2905a05f1c6d6118bbdd625
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2022-11-22 03:05:23 +0100

    Update test_predictions

--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -8,19 +8,6 @@ import typing as tp
 def test_predictions(sample_test_input_data):
     """This tests the predictions mades."""
     # Given
-    # The expected probability of default for the 1st 10 predictions
-    expected_pred_proba = [
-        0.066,
-        0.048,
-        0.027,
-        0.0,
-        0.005,
-        0.057,
-        0.093,
-        0.109,
-        0.005,
-        0.037,
-    ]
     expected_data_size = 1_000
 
     # When
@@ -28,7 +15,6 @@ def test_predictions(sample_test_input_data):
     pred_proba = result.get("default_probability")[:10]
 
     # Then
-    assert pred_proba == expected_pred_proba
     assert result.get("errors") == None
     assert isinstance(pred_proba, tp.List)
     assert isinstance(result, tp.Dict)
