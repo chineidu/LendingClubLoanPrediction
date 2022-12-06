64f05d958135963919e7938f7aed44ce039258e6
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2022-12-06 16:37:14 +0100

    Add evaluation of pipeline

--- classification_model/train_pipeline.py
+++ classification_model/train_pipeline.py
@@ -3,18 +3,19 @@ from config.core import config
 from pipeline import logistic_pipe
 from processing.data_manager import load_data, save_pipeline
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 
 def run_training() -> None:
     """Train the model."""
 
-    # read training data
+    # Read training data
     data = load_data(
         filepath=config.app_config.training_data_file,
         format_=config.model_config.csv_format,
         is_train=True,
     )
-    # divide train and test
+    # Divide train and test
     X_train, X_test, y_train, y_test = train_test_split(
         data[config.model_config.features],  # predictors
         data[config.model_config.target],
@@ -23,11 +24,20 @@ def run_training() -> None:
         random_state=config.model_config.random_state,
     )
 
-    print("===== Training The Pipeline =====")
-    # fit model
+    print("=============== Training The Pipeline ===============")
+    # Fit model
     logistic_pipe.fit(X_train, y_train)
 
-    # persist trained model
+    # Evaluate Model
+    print("=============== Evaluating The Pipeline ===============")
+    train_auc = metrics.roc_auc_score(
+        y_train, logistic_pipe.predict_proba(X_train)[:, 1]
+    )
+    test_auc = metrics.roc_auc_score(y_test, logistic_pipe.predict_proba(X_test)[:, 1])
+
+    print(f"Train_auc_roc: {round(train_auc, 2)}\nTest_auc_roc: {round(test_auc, 2)}\n")
+
+    # Persist trained model
     save_pipeline(pipeline_to_persist=logistic_pipe)
 
 
